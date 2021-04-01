4328c7466bf5f71671ba795f4d99dc52a87377db
Peter Washington <peter@wmkagency.com>
2021-04-01 09:15:39 -0600

    Rolled back to Gatsby 2 so Spotify Source works

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
+    `gatsby-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -28,7 +28,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
+    //`gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
